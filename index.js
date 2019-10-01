35d4925b86e399b2c3d2150a8f49abaa52ced54b
Mic Fox <michael.c.fox.ii@gmail.com>
2019-10-01 14:06:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(list, name) {
+    return list.filter(function (dName){
+        return dName.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(list, partialLetters){
+    let lettersLength = partialLetters.length;
+    return list.filter(function (dName) {
+        return dName.slice(0, lettersLength) === partialLetters;
+    });
+}
+
+function matchName(list, name) {
+    return list.filter(function(driver) {
+        return driver.name.toLowerCase() === name.toLowerCase();
+    });
+}
